88471d22d3cf85c5b5dbcda81ba0689ce194c01f
Bryan Breton <bryanbreton@outlook.fr>
2021-09-04 14:48:31 +0200

    change datas

--- src/store/blog/blog.actions.js
+++ src/store/blog/blog.actions.js
@@ -1,9 +1,25 @@
-import axios from 'axios'
+// import axios from 'axios'
+
+const retour = [
+    {
+        "id": 4,
+        "title": "titre",
+        "content": "Ceci est un contenu",
+        "id_user": 1
+    },
+    {
+        "id": 5,
+        "title": "titre",
+        "content": "Ceci est un contenu",
+        "id_user": 1
+    }
+]
+
 export default {
     getPosts({commit}) {
-        axios.get('http://localhost:8081/posts').then(resp => {
+       /* axios.get('http://localhost:8081/posts').then(resp => {
             commit('getPosts', resp.data)
-        })
-        
+        })*/
+        commit('getPosts', retour)
     }
 }
